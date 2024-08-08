483e2ce5ea7dc18784e9ea01b11ce78bac7de7c7
Steven Frady <sfrady20@gmail.com>
2024-08-07 20:06:41 -0400

    build fix

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,8 @@
 import { db } from "@/services/db";
-import { NextApiHandler } from "next";
 import { NextResponse } from "next/server";
 import * as schema from "@/schema";
 
-export const GET: NextApiHandler = async () => {
+export const GET = async () => {
   const users = await db.select({ name: schema.users.name }).from(schema.users);
   return NextResponse.json(users);
 };
