4f033fd2d2573563935d581b4eed83ed9e7baacd
Steven Frady <sfrady20@gmail.com>
2024-08-07 22:07:04 -0400

    remove users route

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/services/db";
-import { NextResponse } from "next/server";
-import * as schema from "@/schema";
-
-export const GET = async () => {
-  const users = await db.select({ name: schema.users.name }).from(schema.users);
-  return NextResponse.json(users);
-};
